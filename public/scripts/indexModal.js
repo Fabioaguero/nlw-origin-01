d908bdfceed4fa461113d3215c20551823897fac
Fabio Aguero <pablohenriqueceop@gmail.com>
2021-11-08 13:57:05 -0300

    modal
    correçao na funçao do modal

--- public/scripts/indexModal.js
+++ public/scripts/indexModal.js
@@ -2,10 +2,10 @@ const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
 const buttonClose = document.querySelector("#modal .header a")
 
-buttonSearch.addEventListener('click', openMOdal)
+buttonSearch.addEventListener('click', openModal)
 
 
-buttonClose.addEventListener('click', cloceModal)
+buttonClose.addEventListener('click', closeModal)
 
 function openModal(){
   modal.classList.remove('hide')
